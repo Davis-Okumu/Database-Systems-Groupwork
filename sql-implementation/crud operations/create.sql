1d1eaa6703ed0f92a71e5b16459b702eba4445e8
killswitch <166486190+agongadavid@users.noreply.github.com>
2025-04-01 21:16:08 +0300

    attempt 2 at pushing new crud folder operations

---
+++ sql-implementation/crud operations/create.sql
@@ -0,0 +1,3 @@
+-- Create a new applicant
+INSERT INTO Applicants (ApplicantID, FirstName,LastName, DateOfBirth, ContactNumber, Email, HouseholdSize, AnnualIncome, ApplicationStatus)
+VALUES (11, 'John', 'Mwangi', '1990-05-15', '254717305678', 'john.mwangi@gmail.com', 4, 45000, 'Pending')
---
+++ sql-implementation/crud operations/update.sql
@@ -0,0 +1,4 @@
+-- Update an applicant's email
+UPDATE applicants
+SET email = 'jkamau@gmail.com'
+WHERE id = 1;
